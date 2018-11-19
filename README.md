bac42fd2735778b1cc1bcde5ba3c54ad75a15866
OhKanghoon <ggaa96@naver.com>
2018-11-19 16:32:52 +0900

    Add Media Networking

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Tlend
+- 팀명 : Tlend
+- 팀장명 : 오강훈(ggaa96@naver.com)
+- 팀원명 : 김정환(kjtjy41@gmail.com), 김은혜(metinylittledot@gmail.com), 이혜진(knca2@naver.com),이재현(wogusduddnjs@gmail.com)
+- 프로젝트 설명 : Youth고객을 창출하기 위한 아이돌 컨텐츠 기반의 크라우드 펀딩
+- 어플리케이션 구성 : Mobile App, Server
+
+- 운영체제 : iOS
+- 개발언어 : swift
+
+- 운영기반 : clayon platform PAS, aws
+- 사용서비스 : clayon App Autoscaler, aws rds Mysql service, aws s3 service, aws CloudFront service
---
+++ Tlend/Tlend/ViewController/Content/ContentDetailViewController.swift
@@ -0,0 +1,143 @@
+//
+//  ContentDetailViewController.swift
+//  Tlend
+//
+//  Created by KanghoonOh on 2018. 11. 19..
+//  Copyright © 2018년 LeeHyeJin. All rights reserved.
+//
+
+import UIKit
+import MMPlayerView
+import AVFoundation
+
+class ContentDetailViewController: UIViewController {
+    
+    enum Section: Int, CaseIterable {
+        case image
+        case video
+        case header
+        case items
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    lazy var mmPlayerLayer: MMPlayerLayer = {
+        let l = MMPlayerLayer()
+        
+        l.cacheType = .memory(count: 1)
+        l.coverFitType = .fitToPlayerView
+        l.videoGravity = AVLayerVideoGravity.resizeAspect
+        l.replace(cover: CoverA.instantiateFromNib())
+        return l
+    }()
+    
+    var mediaID: Int?
+    var itemList: [TlendItem] = []
+    var detailMedia: DetailMedia?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getData()
+    }
+    
+    private func getData() {
+        ContentService.shared.getMedia(mediaID: mediaID ?? 0) { [weak self] result in
+            switch result {
+            case .success(let data):
+                self?.itemList = data.itemList
+                self?.detailMedia = data.media
+                self?.tableView.reloadData()
+            case .error(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension ContentDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = Section(rawValue: section) else { return 0 }
+        switch section {
+        case .image, .video, .header:
+            return 1
+        case .items:
+            return itemList.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let section = Section(rawValue: indexPath.section) else { return UITableViewCell() }
+        switch section {
+        case .image:
+            guard let media = detailMedia else { return UITableViewCell() }
+            let cell = tableView.dequeue(ContentImageTableViewCell.self, for: indexPath)
+            cell.delegate = self
+            cell.configure(media.detailImage)
+            return cell
+        case .video:
+            guard let media = detailMedia else { return UITableViewCell() }
+            let cell = tableView.dequeue(ContentVideoTableViewCell.self, for: indexPath)
+            mmPlayerLayer.thumbImageView.image = cell.videoImageView.image
+
+            if !MMLandscapeWindow.shared.isKeyWindow {
+                mmPlayerLayer.playView = cell.videoImageView
+            }
+            
+            mmPlayerLayer.set(url: URL(string: media.videoKey))
+            mmPlayerLayer.resume()
+            return cell
+        case .header:
+            return tableView.dequeueReusableCell(withIdentifier: "ContentItemHeader", for: indexPath)
+        case .items:
+            guard itemList.count > indexPath.row else { return UITableViewCell() }
+            let cell = tableView.dequeue(ContentItemTableViewCell.self, for: indexPath)
+            cell.configure(itemList[indexPath.row])
+            return cell
+        }
+
+    }
+}
+
+extension ContentDetailViewController: ContentImageProtocol {
+    func setImageHeight() {
+        self.tableView.beginUpdates()
+        self.tableView.endUpdates()
+    }
+}
+
+extension ContentDetailViewController: MMPlayerFromProtocol {
+    // when second controller pop or dismiss, this help to put player back to where you want
+    // original was player last view ex. it will be nil because of this view on reuse view
+    func backReplaceSuperView(original: UIView?) -> UIView? {
+        return original
+    }
+    
+    // add layer to temp view and pass to another controller
+    var passPlayer: MMPlayerLayer {
+        return self.mmPlayerLayer
+    }
+    // current playview is cell.image hide prevent ui error
+    func transitionWillStart() {
+        self.mmPlayerLayer.playView?.isHidden = true
+    }
+    // show cell.image
+    func transitionCompleted() {
+        self.mmPlayerLayer.playView?.isHidden = false
+    }
+    
+    func dismissViewFromGesture() {
+        mmPlayerLayer.thumbImageView.image = nil
+    }
+    
+    func presentedView(isShrinkVideo: Bool) {
+        self.tableView.visibleCells.forEach {
+            if ($0 as? ContentVideoTableViewCell)?.videoImageView.isHidden == true && isShrinkVideo {
+                ($0 as? ContentVideoTableViewCell)?.videoImageView.isHidden = false
+            }
+        }
+    }
+}
