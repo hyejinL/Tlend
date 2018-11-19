a9bd3aea6aa56d6e77c93acf48080d8887a2ce8f
l-hyejin <knca2@naver.com>
2018-11-20 01:46:49 +0900

    add finish funding view layout

---
+++ Tlend/Tlend/View/Detail/Funding End/BuyerInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BuyerInfoTableViewCell.swift
+//  Tlend
+//
+//  Created by 이혜진 on 2018. 11. 20..
+//  Copyright © 2018년 LeeHyeJin. All rights reserved.
+//
+
+import UIKit
+
+class BuyerInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
