f6e4599709dddea2301f3bfbe6ca06d6b40223fd
l-hyejin <knca2@naver.com>
2018-11-20 11:45:53 +0900

    add home navigation color animation

--- Tlend/Tlend/Util/Extension/UIViewController+NavigationBar.swift
+++ Tlend/Tlend/Util/Extension/UIViewController+NavigationBar.swift
@@ -23,4 +23,46 @@ extension UIViewController {
         guard let navigationController = UIStoryboard(name: "Search", bundle: nil).instantiateViewController(withIdentifier: "SearchNavigation") as? UINavigationController else { return }
         self.present(navigationController, animated: true, completion: nil)
     }
+    
+    // View controller-based status bar appearance
+    func setNavigationWhenDidScroll(_ scrollView: UIScrollView,
+                                    underNavi view: UIView,
+                                    completion: ((CGPoint) -> Void)?) {
+        let offset = scrollView.contentOffset
+        guard offset.y >= 20 else {
+            view.isHidden = true
+            UIApplication.shared.statusBarStyle = .lightContent
+            return
+        }
+        let ratio: CGFloat = (300-offset.y)/300
+        view.isHidden = false
+        view.alpha = 1.0-ratio
+        
+        if offset.y > 300 {
+            UIApplication.shared.statusBarStyle = .default
+        } else {
+            UIApplication.shared.statusBarStyle = .lightContent
+        }
+        setNeedsStatusBarAppearanceUpdate()
+        
+        let color: CGFloat = ratio*255
+//        guard color >= 0 else {
+//            color = 0
+//            return
+//        }
+        let buttonColor = UIColor(red: color, green: color, blue: color, alpha: 1.0)
+        
+        if let buttons = self.navigationItem.leftBarButtonItems {
+            for button in buttons {
+                button.tintColor = buttonColor
+            }
+        }
+        if let buttons = self.navigationItem.rightBarButtonItems {
+            for button in buttons {
+                button.tintColor = buttonColor
+            }
+        }
+        
+        completion?(offset)
+    }
 }
